57d19b378237c2c2f64f843b68e8458ac7da7c72
manniig24 <egatica51@gmail.com>
2020-02-02 22:44:16 -0700

    Added comments

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import PageNotFound from './components/PageNotFound';
 import apiKey from './config';
 
 class App extends Component {
-  
+  // Everytime the app opens up we want to create a new array to store photos and set loading to true
   constructor() {
     super();
     this.state = {
@@ -24,12 +24,17 @@ class App extends Component {
       loading: true
     };
   } 
+
+  // When the app starts we want to run this function that calls the function that runs the search as well as keeps track of history
   componentDidMount() {
     const history = createBrowserHistory();
     let query = history.location.pathname.replace(/[^\w\s]/gi, '').replace("search", '');
     console.log(history);
     this.performSearch(query);
   }
+
+  // This function calls the api using the api key from the config component and the parameter passed from component did mount
+  // If no parameter is specified the default value is cat
   performSearch = (query = 'cats') => {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
@@ -43,11 +48,15 @@ class App extends Component {
     });
   }
 
-
+// Renders components based on url/links
   render() { 
+
+    // Using this for testing erase later
     console.log(this.state.photos);
+
     return (
-      <BrowserRouter>
+      
+      <BrowserRouter> 
         <div className="container">
           <Route render={(props)=>
             <SearchForm onSearch={this.performSearch} {...props}/>
